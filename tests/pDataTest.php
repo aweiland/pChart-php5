0da8b623f6807aa04ca959677474452e8bac189a
Tim Martin <tim@asymptotic.co.uk>
2010-10-14 10:11:13 +0200

    Began adding unit tests

---
+++ tests/pDataTest.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+require_once 'lib/pData.php';
+
+class pDataTest extends PHPUnit_Framework_TestCase {
+	public function testAddPoint() {
+		$data = new pData();
+
+		$data->addPoint(array(3 => 4, 4 => 5));
+
+		/* NB: Key values passed in the array to addPoint are
+		 discarded */
+		$this->assertEquals(array(0 => array('Serie1' => 4,
+											 'Name' => 0),
+								  1 => array('Serie1' => 5,
+											 'Name' => 1)),
+							$data->getData());
+	}
+}
